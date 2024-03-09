c292d41f1b06af9cbea7e67426b82e5b38eba31d
Rohan Dev Singh <108291771+irohandev@users.noreply.github.com>
2024-03-09 17:18:01 +0530

    Update

---
+++ 04_CONTROL_FLOW/01_if_else.js
@@ -0,0 +1,35 @@
+// if(condititon){
+
+// }                    //Syntax of if else in javascript
+// else{
+
+// }
+
+
+
+// if(condititon){
+
+// }  
+// else if(condition){
+
+// }   
+// else if(){
+
+// }            //Syntax of if else in javascript
+// else{
+
+// }
+
+
+const userlogin = true
+const gmaillogin = true
+if (userlogin || gmaillogin){           // yeh hai or {||} ka sign jisse kya hota hai ki dono mein se koi ek koi ek true hone se kam further code execute ho jyega 
+    console.log("User can shop now!!");
+}
+else {
+    console.log("User cannot shop now, LOGIN AGAIN");
+}
+
+
+// NOTE:
+// 1. && yeh hai AND agar yeh conditional statement ke andar raha toh that means jitna bhi ho conditional ho sab true hona pdega.....wrna there will be no execution
