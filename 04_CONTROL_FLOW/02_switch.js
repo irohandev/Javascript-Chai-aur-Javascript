c53446cc77675bedcb805634c02b59edb7f48f0d
Rohan Dev Singh <108291771+irohandev@users.noreply.github.com>
2024-03-09 17:48:27 +0530

    Update

---
+++ 04_CONTROL_FLOW/02_switch.js
@@ -0,0 +1,26 @@
+const day = 6
+switch(day){
+    case 1:
+        console.log("Sunday");
+        break
+    case 2:
+        console.log("Monday");
+        break
+    case 3:
+        console.log("Tuesday");
+        break
+    case 4:
+        console.log("Wednesday");
+        break
+    case 5:
+        console.log("Thrusday");
+        break
+    case 6:
+        console.log("Friday");
+        break
+    case 7:
+        console.log("Saturay");
+        break
+    default:
+        console.log("Wrong day selected !");
+}
