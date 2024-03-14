ef0a800a5ed1937e95b0859d4a47fcd9fb6253d7
Rohan Dev Singh <108291771+irohandev@users.noreply.github.com>
2024-03-14 12:40:47 +0530

    Updated

--- 07_PROJECT/01_Project1/script.js
+++ 07_PROJECT/01_Project1/script.js
@@ -2,7 +2,6 @@ const buttons = document.querySelectorAll('.button')
 const body = document.querySelector('body')
 
 buttons.forEach(function(button){
-    console.log(button);
     button.addEventListener('click', function(e) {
         console.log(e);
         console.log(e.target);
@@ -16,6 +15,9 @@ buttons.forEach(function(button){
             body.style.backgroundColor = e.target.id            
         }
         if (e.target.id === 'white') {
+            body.style.backgroundColor = e.target.id            
+        }
+        if (e.target.id === 'purple') {
             body.style.backgroundColor = e.target.id            
         }
         
