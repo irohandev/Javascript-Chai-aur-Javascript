6077c64c60be816a6d189e2831109d0a522163b7
Rohan Dev Singh <108291771+irohandev@users.noreply.github.com>
2024-03-22 20:51:14 +0530

    Update

---
+++ 10_OOPS/11_Properties_getter_setter.js
@@ -0,0 +1,28 @@
+//previously used getter & setter syntax
+
+function User(email, password){
+    this._email = email;
+    this._password = password
+
+    Object.defineProperty(this, 'email', {
+        get: function(){
+            return this._email.toUpperCase()
+        },
+        set: function(value){
+            this._email = value
+        }
+    })
+    Object.defineProperty(this, 'password', {
+        get: function(){
+            return this._password.toUpperCase()
+        },
+        set: function(value){
+            this._password = value
+        }
+    })
+
+}
+
+const val = new User("abc@google.com", "password")
+
+console.log(val.email);
