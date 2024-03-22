fec041678a010a6d07223c0d462c8a66407d5eb4
Rohan Dev Singh <108291771+irohandev@users.noreply.github.com>
2024-03-22 20:46:14 +0530

    Update

---
+++ 10_OOPS/10_Getter_&_setter.js
@@ -0,0 +1,25 @@
+class User {
+    constructor(email, password){
+        this.email = email;
+        this.password = password
+    }
+
+    get email(){            //getter aur setter k name values ke equal hoga
+        return this._email.toUpperCase()
+    }
+    set email(value){               //get ke sath hmesa set ko bhi add krna pdega
+        this._email = value
+    }
+
+    get password(){
+        return `${this._password}rohan`         //constructor k andr ke value ko override krna hai toh idhar values ko chnge krna pdega..wrna error ayega 
+    }
+
+    set password(value){
+        this._password = value
+    }
+}
+
+const val = new User("rohan@ai.com", "abc")
+console.log(val.password);
+console.log(val.email);
